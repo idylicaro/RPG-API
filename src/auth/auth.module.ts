b002aa2765aacf8b817cd5a7160d352a1f2ab26d
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-09-10 19:18:32 -0300

    create local auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './shared/auth.service';
+import { LocalStrategy } from './shared/local.strategy';
 
 @Module({
   imports: [UsersModule],
   controllers: [],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
