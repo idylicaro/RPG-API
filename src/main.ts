79cb02ca1a75cf385875a0e59aaf330e108d8692
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-09-10 14:33:10 -0300

    config Validation Pipe to use whitelist, forbidNonWhitelisted, transform

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Deletes all data beyond the 'Dto' of the route
+      forbidNonWhitelisted: true, // Prevents you from continuing with the request if you pass some unnecessary information in the 'Dto'
+      transform: true, // Converts all data received from the route into a 'Dto'
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
