ecfadd0299a78b324a926e5ee7a6ff16c042d46f
Idyl Ícaro dos Santos <idylicaro.se@hotmail.com>
2022-01-17 16:11:15 -0300

    fix: resolve #13 #14

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CharactersModule } from './characters/characters.module';
-import { CategoryController } from './category/category.controller';
 import { CategoryModule } from './category/category.module';
 
 @Module({
