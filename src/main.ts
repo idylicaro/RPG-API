37438fa6d7e626bb42a6fd82db612de562346f89
Idyl Ícaro dos Santos <idylicaro.se@hotmail.com>
2022-01-17 17:15:20 -0300

    feat: add authorization in routes, resolve issue #15

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port);
+    console.log("Express server listening on port %d in %s mode", process.env.PORT || 3000);
   });
 }
 bootstrap();
